e52a36ba99e92bf7aaa2a9d31d832161fba3cedb
andela-gnyenyeshi <gertrude.nyenyeshi@andela.com>
2019-04-16 15:23:59 +0300

    generate unique key

--- services/cache.js
+++ services/cache.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function() {
-    console.log('YEZZZUUUR');
+    const key = Object.assign({}, this.getQuery(), {
+        collection: this.mongooseCollection.name
+    });
+
+    console.log(key)
+
     return exec.apply(this, arguments);
 }
