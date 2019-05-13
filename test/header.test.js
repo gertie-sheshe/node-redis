2cacffd3f59d8374b7a0c365480d5873c255df10
andela-gnyenyeshi <gertrude.nyenyeshi@andela.com>
2019-05-13 18:54:00 +0300

    launch and terminate browser instance

--- test/header.test.js
+++ test/header.test.js
@@ -1,15 +1,22 @@
 const puppeteer = require('puppeteer');
+let browser, page;
 
-test('Adds two numbers', () => {
-    const sum = 1 + 2;
-
-    expect(sum).toEqual(3);
-});
-
-test('Launch browser', async () => {
-    const browser = await puppeteer.launch({
+beforeEach(async () => {
+    browser = await puppeteer.launch({
         headless: false
     });
 
-    const page = await browser.newPage();
+    page = await browser.newPage();
+    await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+    await browser.close();
+});
+
+test('Launch browser', async () => {
+    
+    const text = await  page.$eval('a.brand-logo', el => el.innerHTML);
+
+    expect(text).toEqual('Blogster');
 });
