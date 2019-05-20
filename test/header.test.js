0c7801ef42f09bf77e0a7dc078eeb291626910be
andela-gnyenyeshi <gertrude.nyenyeshi@andela.com>
2019-05-20 04:09:51 +0300

    pull session helper in test

--- test/header.test.js
+++ test/header.test.js
@@ -1,6 +1,5 @@
 const puppeteer = require('puppeteer');
-const Keygrip = require('keygrip');
-const keys = require('../config/keys');
+const sessionFactory = require('./factories/sessionFactory');
 
 let browser, page;
 
@@ -32,28 +31,20 @@ test('Clicking Login starts OAuth flow', async () => {
 
 test('When signed in, show logout button', async () => {
     const id = '5caf06d0bff2bb270e92fb87';
-    const Buffer = require('safe-buffer').Buffer;
-    const sessionObject = {
-        passport: {
-            user: id
-        }
-    };
-
-    // Convert id to base64 session string, that can
-    // be decoded by passport back into the sessionObject
-    const sessionString = Buffer
-    .from(JSON.stringify(sessionObject))
-    .toString('base64');
-
-    const keygrip = new Keygrip([keys.cookieKey]);
-
-    // Sign the session
-    const sig = keygrip.sign('session=' + sessionString);
+    const {session, sig} = sessionFactory(id);
 
     // Set the values in the cookie.
-    await page.setCookie({name: 'session', value: sessionString});
+    await page.setCookie({name: 'session', value: session});
     await page.setCookie({name: 'session.sig', value: sig});
 
     // Refresh page so values are set in cookie
     await page.goto('localhost:3000');
+
+    // Wait for page to completely load
+    // And element to show
+    await page.waitFor('a[href="/auth/logout"]');
+
+    // Check that Logout button now shows
+    const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+    expect(text).toEqual('Logout');
 });
