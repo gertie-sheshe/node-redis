be9014623129b371cbf3d6ac9166486a833618eb
andela-gnyenyeshi <gertrude.nyenyeshi@andela.com>
2019-05-21 05:59:17 +0300

    test blog creation error

--- test/blog.test.js
+++ test/blog.test.js
@@ -16,10 +16,28 @@ afterEach(async () => {
     await browser.close();
 });
 
-test('When logged in, can see blog create form', async () => {
-    await loginPage(page);
-    await page.click('a.btn-floating');
-    const label = await page.$eval('form label', el => el.innerHTML);
+describe('When logged in', async () => {
+    beforeEach(async () => {
+        await loginPage(page);
+        await page.click('a.btn-floating');
+    });
 
-    expect(label).toEqual('Blog Title');
+    test('Can see blog create form', async () => {
+        const label = await page.$eval('form label', el => el.innerHTML);
+        expect(label).toEqual('Blog Title');
+    });
+
+    // Nested describe
+    describe('And using invaid input', async () => {
+        beforeEach(async () => {
+            await page.click('form button');
+        });
+        test('the form shows an error message', async () => {
+            const titleError = await page.$eval('.title .red-text', el => el.innerHTML);
+            const contentError = await page.$eval('.content .red-text', el => el.innerHTML);
+
+            expect(titleError).toEqual('You must provide a value');
+            expect(contentError).toEqual('You must provide a value');
+        });
+    });
 });
