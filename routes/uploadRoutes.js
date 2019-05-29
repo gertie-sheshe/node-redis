f9ea5d092cefa37c68f6c1aa7b5ebf6597530412
andela-gnyenyeshi <gertrude.nyenyeshi@andela.com>
2019-05-29 10:14:43 +0300

    aws image upload logic

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,5 +1,19 @@
+const AWS = require('aws-sdk');
+const uuid = require('uuid/v1');
+const keys = require('../config/keys');
+
+const s3 = AWS.S3({
+    accessKeyId: keys.accessKeyId,
+    secretAccessKey: keys.secretAccessKey
+});
+
 module.exports = (app) => {
  app.get('/api/upload', () => {
-     
+     const key = `${req.user.id}/${uuid()}.jpeg`
+    s3.getSignedUrl('putObject', {
+        Bucket: 'gertie-blog-bucket-123',
+        ContentType: 'jpeg',
+
+    })
  });
 }
