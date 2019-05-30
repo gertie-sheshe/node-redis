2c5b0f3734197ba8048251a57df3e02b4f52e8f5
andela-gnyenyeshi <gertrude.nyenyeshi@andela.com>
2019-05-30 11:00:06 +0300

    AWS logic

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -13,7 +13,19 @@ export const handleToken = token => async dispatch => {
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const submitBlog = (values, history) => async dispatch => {
+export const submitBlog = (values, file, history) => async dispatch => {
+  const uploadConfig = await axios.get('/api/upload');
+
+  try {
+    await axios.put(uploadConfig.data.url, file, {
+      headers: {
+        'Content-Type': file.type
+      }
+    });
+  } catch(err) {
+    console.log('Error', err);
+  }
+
   const res = await axios.post('/api/blogs', values);
 
   history.push('/blogs');
--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -3,18 +3,20 @@ const uuid = require('uuid/v1');
 const requireLogin = require('../middlewares/requireLogin');
 const keys = require('../config/keys');
 
-const s3 = AWS.S3({
+const s3 = new AWS.S3({
     accessKeyId: keys.accessKeyId,
     secretAccessKey: keys.secretAccessKey
 });
 
 module.exports = (app) => {
- app.get('/api/upload', requireLogin, () => {
-     const key = `${req.user.id}/${uuid()}.jpeg`
+    console.log('SERVER SIDE IMAGE');
+ app.get('/api/upload', requireLogin, (req, res) => {
+     const key = `${req.user.id}/${uuid()}.jpeg`;
+
     s3.getSignedUrl('putObject', {
         Bucket: 'gertie-blog-bucket-123',
-        ContentType: 'jpeg',
-
-    })
+        ContentType: 'image/jpeg',
+        Key: key
+    }, (err, url) => res.send({key, url}));
  });
 }
